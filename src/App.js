dc780e887b61fb7c842c399816812c674e5fe83c
PJ Murray <pj.murray.nz@gmail.com>
2021-09-13 09:35:31 +1000

    almost done

--- src/App.js
+++ src/App.js
@@ -11,10 +11,14 @@ import mouse from './imgs/mouse.png'
 import mouseClicked from './imgs/clicked.png'
 import buddha from './imgs/buddha.png'
 import buddha2 from './imgs/buddha2.png'
+import deli from './imgs/deli.png'
+import llama from './imgs/llama.png'
 import kumquat from './imgs/kum.png'
 import tree from './imgs/tree.png'
+import spoon from './imgs/spoon.jpeg'
+import sunglasses from './imgs/sunglasses.png'
 
-const PASSWORD = 'peach'
+import Typist from 'react-typist';
 
 // buddha 
 // - before I show you the way, first I need you help
@@ -38,16 +42,85 @@ const COPY = {
     "To find ourselves, we must first get lost.",
     "What is it you seek, my child?",
   ],
-  budda: [
-
+  buddha: [
+    [
+      "My young disciple",
+      "Welcome.",
+      "",
+      "You are weary, and have come far.",
+      "These times have been tough, and unfair.",
+      "I can see your hurting",
+      "",
+      "",
+      "But I am here to help.",
+      "I am here to set you free.",
+    ],
+    [ 
+      "You see, We are all seeking something",
+      "",
+      "However…",
+      '',
+      "I have met many who seek",
+      "But few who have found",
+      '',
+      "Many who want",
+      "But few who need",
+      '',
+      "Many who take",
+      "But few who give",
+      "",
+      "Before I show you the path forward,",
+      "You must surrender yourself to service.",
+      "",
+      "Are you ready?"
+    ],
+   [
+    'In order to receive, we must first learn to give.',
+    '',
+    'I have a good friend, the Deli Llama, who needs your help',
+    '',
+    'The seasons have been kind to him',
+    'And he has been met with abundance',
+    'But he is old and frail',
+    'He is unable to reep the benefits,',
+    'of the seeds he once sowed',
+    '',
+    'Will you help him?'
+  ]
   ],
-  story: [
-    "My young disciple",
-    "I have met many who seek",
-    "But few who have found",
-    "Plenty have gone mad in the persuit of {{seeking}}",
-    "For they lack the patience and",
-
+  deli_1: [
+    "Welcome child, to my Deli",
+    "Your offer of help is very kind.",
+    '',
+    '',
+    "This summer has been good to me,",
+    "and my farm has bore many fruits.",
+    "But I dont have the means to harvest.",
+    "Can you please help me?",
+  ],
+  deli_2: [
+    "Thank you!",
+    "Come with me...."
+  ],
+  game_1: [
+    'Welcome to my Kumquat farm',
+    'I need you to help me pick my fruit',
+    'But my fruit are gentle,',
+    'and must be handled with care.',
+    'Please select an appropriate tool…',
+  ],
+  game_2: [
+    "Yes, that will do nicely!",
+    "Please harvest my fruit for me",
+  ],
+  game_3: [
+    "Amazing - now I can get back to work",
+    "Thank you kind wanderer.",
+  ],
+  premap: [
+      'Thank you for helping my good friend',
+      "You have proven you are ready",
+      "Ready to be shown the path",
   ],
   map: [
   "They speak of a place…",
@@ -56,6 +129,12 @@ const COPY = {
   "…the air blows fresh…",
   "…and the only things that's missing…",
   "Is you"
+  ],
+  map_2: [
+    "You're being called",
+    "To join on an adventure",
+    "On an journey with no destination",
+    "A beginning with no end",
   ],
   announcement: [
     
@@ -173,47 +252,156 @@ return (
 );
 };
 
+const Story = ({onDone, onTypingDone, startDelay=2000, copy, cta}) => {
+  return <div>
+     <Typist cursor={{show: false}} onTypingDone={() => onTypingDone && onTypingDone()} startDelay={startDelay} avgTypingDelay={40}>
+    {copy.map(c => c === '' ? <br/> : <span><p>{c}</p><Typist.Delay ms={600} /></span>)}
+    <Typist.Delay ms={1000} />
+    {cta ? <button className='inline-block mt-10 cursor-pointer border border-monk px-4 py-2 hover:bg-monk hover:text-white' onClick={onDone}>{cta}</button> : <p></p>}
+    </Typist>
+  </div>
+}
 const Buddha = ({onSuccess}) => {
   const [ show, setShow ] = useState(false)
+  const [ step, setStep ] = useState(1)
   const copy = COPY.buddha
   useEffect(() => {
     setShow(true)
   }, [])
-  return (<div className={"h-full w-full flex flex-col items-center justify-center p-20 bg-white text-black cursor-none"}>
-    <Fade show={show}>
-    <img src={buddha2} alt="buddha" className='animate-float' width="400"/>
+  return (
+    <>
+    {step === 1 &&  <BlackFade onDone={() => setStep(2)} from/>}
+    <Fade show={step > 1} className='w-1/2'>
+      <img src={buddha2} alt="buddha" className='animate-float' width="400"/>
     </Fade>
-    <div class='pt-10'>
-    <p>We are all seeking something</p>
+    <div className='w-1/2'>
+    { step === 2 &&  <Story copy={copy[0]} cta="Yes…" onDone={() => setStep(3)}/>}
+    { step === 3 &&  <Story copy={copy[1]} cta="Yes, I'm ready" onDone={() => setStep(4)}/>}
+    { step === 4 &&  <Story copy={copy[2]} cta="Sure" onDone={() => setStep(5)}/>}
+    { step === 5 &&  <BlackFade onDone={onSuccess}/>}
     </div>
-    </div>)
+    </>)
 }
 
-const Kumquat = ({x=0, y=0}) => {
-  const [clicked, setClicked] = useState()
-  return <img className={`${clicked ? 'animate-spin' : ''} absolute z-40 transition-all duration-4000 w-6`} style={{top: clicked ? 999 : y, left: x}} src={kumquat} alt='kumquat' onClick={() => setClicked(true)}/>
+const Premap = ({onSuccess}) => {
+  const [ step, setStep ] = useState(1)
+  const copy = COPY.premap
+  return (
+    <>
+    { step === 1 && <BlackFade onDone={() => setStep(2)} from/>}
+    { step === 3 && <BlackFade onDone={onSuccess}/>}
+    <Fade show={step > 1} className='w-1/2'>
+    <img src={buddha2} alt="buddha" className='animate-float' width="400"/>
+    </Fade>
+    <div className='w-1/2'>
+    { step === 2 &&  <Story copy={copy} cta='Please get on with it' onDone={() => setStep(3)}/>}
+    </div>
+    </>)
+}
+
+const Deli = ({onSuccess}) => {
+  const [ show, setShow ] = useState(false)
+  const [step, setStep] = useState(1)
+  useTimeout(() => {
+    setShow(true)
+  }, 1000)
+  return (
+    <>
+    { step === 1 && <BlackFade onDone={() => setStep(2)} from/>}
+    { step === 4 && <BlackFade onDone={onSuccess}/>}
+    <div className='w-1/4'></div>
+    <div className='w-1/4'>
+      {step === 2 && <Story copy={COPY.deli_1} startDelay={10000} onDone={() => setStep(3)} cta="Yes"/>}
+      { step === 3 && <Story copy={COPY.deli_2} onTypingDone={() => setStep(4)}/> }
+    </div>
+    <div className='w-1/2 relative flex flex-row items-center justify-center'>
+    <img src={llama} alt="deli" className='animate-llama transition-all relative duration-10000' width={300} style={{left: step > 1 ? 0: 600}}/> 
+    <img src={deli} alt="deli" className='absolute top-60'/>
+    </div>
+    </>)
+}
+
+const Kumquat = ({x=0, y=0, enabled=false, onClick, clicked}) => {
+  return <div className='-mt-2 -ml-2 p-1' onClick={() => enabled &&  onClick()}><img className={`${clicked ? 'animate-spin' : ''} absolute z-40 transition-all duration-4000 w-6`} style={{top: clicked ? 2999 : y, left: x}} src={kumquat} alt='kumquat' /></div>
 } 
 
-const KumquatTree = ({style}) => {
+const KumquatTree = ({onPicked, enabled,style}) => {
+  const [clicked, setClicked] = useState({})
+
+  useEffect(() => {
+    const vals = Object.values(clicked)
+    if (vals.length === 5 && vals.every(v => v)){
+      onPicked()
+    }
+  },[onPicked, clicked])
+
+  function handleClicked (i) {
+    setClicked({[i]: true, ...clicked})
+  }
   return <div className='absolute w-48' style={style}>
-    <Kumquat x={30} y={40}/>
-    <Kumquat x={145} y={45}/>
-    <Kumquat x={30} y={100}/>
-    <Kumquat x={85} y={25}/>
-    <Kumquat x={130} y={95}/>
+    <Kumquat clicked={clicked[0]} onClick={() => handleClicked(0)} enabled={enabled} x={30} y={40}/>
+    <Kumquat clicked={clicked[1]} onClick={() => handleClicked(1)} enabled={enabled} x={145} y={45}/>
+    <Kumquat clicked={clicked[2]} onClick={() => handleClicked(2)} enabled={enabled} x={30} y={100}/>
+    <Kumquat clicked={clicked[3]} onClick={() => handleClicked(3)} enabled={enabled} x={85} y={25}/>
+    <Kumquat clicked={clicked[4]} onClick={() => handleClicked(4)} enabled={enabled} x={130} y={95}/>
     <img src={tree} alt='tree' className='w-full absolute'/>
     </div>
 
 }
 
-const Game = () => {
+const KumquatTrees = ({enabled, onPicked}) => {
+  const [picked, setPicked] = useState({})
+
+  useEffect(() => {
+    const vals = Object.values(picked)
+    if (vals.length === 5 && vals.every(v => v)){
+      onPicked()
+    }
+  },[onPicked, picked])
+
+  function handlePicked (i) {
+    setPicked({[i]: true, ...picked})
+  }
+  return <>
+    <KumquatTree onPicked={() => handlePicked(0)} enabled={enabled} style={{top: 150, left: 50 }}/>
+  <KumquatTree  onPicked={() => handlePicked(1)} enabled={enabled} style={{top: 50, left: 300 }}/>
+  <KumquatTree  onPicked={() => handlePicked(2)} enabled={enabled} style={{top: 100, right: 200 }}/>
+  <KumquatTree  onPicked={() => handlePicked(3)} enabled={enabled} style={{bottom: 300, right: 300 }}/>
+  <KumquatTree  onPicked={() => handlePicked(4)} enabled={enabled} style={{bottom: 300, left: 300 }}/>
+  </>
+}
+
+const Game = ({onSuccess}) => {
+  const [step, setStep] = useState(0)
+  const [ error, setError] = useState()
+  const canPlay = step > 2
   return <div className='overflow-hidden max-h-full'>
-  <ToeMouse/>
-  <KumquatTree style={{top: 150, left: 50 }}/>
-  <KumquatTree style={{top: 50, left: 300 }}/>
-  <KumquatTree style={{top: 100, right: 200 }}/>
-  <KumquatTree style={{bottom: 300, right: 300 }}/>
-  <KumquatTree style={{bottom: 300, left: 300 }}/>
+  {canPlay && <ToeMouse/>}
+  { step === 0 && <BlackFade onDone={() =>  setStep(1)} from/>}
+  <div className='flex flex-row space-y-4'>
+  { step === 0 &&  <div class='w-96 mt-10'/>}
+  { step === 1 &&  <div class='w-96 mt-10'>
+    <Story copy={COPY.game_1} cta="Okay" onDone={() => setStep(2)}/>
+  </div>}
+  { step === 2 &&  <div className='w-96 mt-10 '> <div className='grid grid-cols-2 text-center'>
+    <img src={mouse} alt='toe' className='h-24' onClick={() => setStep(3)}/>
+    <img src={spoon} alt='spoon' className='h-24' onClick={() => setError('That spoon is far to small to pick a kumquat with!')}/>
+    <img src={sunglasses} alt='glasses' className='h-24' onClick={() => setError('Sunglasses my look good on a wig, but they are no way to pick a kumquat!')}/>
+    <img src={sunglasses} alt='glasses' className='h-24' onClick={() => setError('Sunglasses my look good on a wig, but they are no way to pick a kumquat!')}/>
+
+    </div>
+    {error && <p className='mx-10 mt-10'>{error}</p>}
+  </div>}
+  { step === 3 &&  <div className='w-96 mt-10'>
+    <Story startDelay={0} copy={COPY.game_2}/>
+  </div>}
+  { step === 4 &&  <div className='w-96 mt-10'>
+    <Story startDelay={0} copy={COPY.game_3} onTypingDone={() => setStep(5)}/>
+  </div>}
+  { step === 5 && <BlackFade onDone={onSuccess}/>}
+    <img src={llama} alt='llama' width={200} /> 
+  </div>
+    <KumquatTrees enabled={canPlay} onPicked={() => setStep(4)}/>
   </div>
 }
 
@@ -226,9 +414,19 @@ const Hover  = ({className='', setHover, value, children}) => {
     </div>)
 }
 
-const Announcement = ({showAll}) => {
+const BlackFade = ({duration= 2000, onDone, from=false}) => {
+  const [black, setBlack] = useState(from)
+  useEffect(() => {
+    setBlack(!from)
+  }, [])
+
+  useTimeout(() => {onDone()}, 2000)
+  return <div className={`absolute z-50 inset-0 transition-colors duration-${duration} ${(black ? 'bg-black' : 'bg-transparent')}`}/>
+}
+
+const Announcement = () => {
   const [width, setWidth] = useState(200)
-  const [show, setShow] = useState(showAll ? [true, true, true, true,true] : [])
+  const [show, setShow] = useState([])
   const [hover, setHover] = useState()
   useEffect(() => {
     setWidth(800)
@@ -326,46 +524,27 @@ const Announcement = ({showAll}) => {
   </Screen>
 }
 
-const Details = () => {
-  return <Screen className='flex-row'>
-    <div className='flex flex-row'>
-      <div>
-        What
-      </div>
-      <div>
-        When
-      </div>
-      <div>
-        Where
-      </div>
-      <div>
-        Who
-      </div>
-    </div>
-    <div>Interested?</div>
-  </Screen>
-}
-
-const Screen = ({className="bg-black", children}) => {
-return (<div className={`${className} h-full w-full font-buda text-xl flex items-center justify-center text-white text-center`}>
+const Screen = ({className="bg-black text-white", children}) => {
+return (<div className={`${className} h-full w-full font-buda text-xl flex items-center justify-center text-center`}>
   {children}
 </div>)
 
 }
 function App() {
   const [store, setStore] = useState()
-  const [page, setPage ] = useState('annoucement')
+  const [page, setPage ] = useState('login')
   return (
     <>
       { page === 'login' && <Screen><Login setStore={setStore} onSuccess={({password}) => {
         setStore({password, ...store})
         setPage('buddha')
         }}/></Screen>}
-      { page === 'buddha' && <Screen className='bg-white'><Buddha/></Screen>}
-      { page === 'game' && <Screen className='bg-white'><Game onSuccess={() => setPage('info')}/></Screen>}
+      { page === 'buddha' && <Screen className='bg-white text-black'><Buddha onSuccess={() => setPage('deli')}/></Screen>}
+      { page === 'deli' && <Screen className='bg-white text-black'><Deli onSuccess={() => setPage('game')}/></Screen>}
+      { page === 'game' && <Screen className='bg-white text-black'><Game onSuccess={() => setPage('premap')}/></Screen>}
+      { page === 'premap' && <Screen className='bg-white text-black'><Premap onSuccess={() => setPage('map')}/></Screen>}
       { page === 'map' && <Map onSuccess={() => setPage('info')}/>}
-      { page === 'annoucement' && <Announcement showAll/>}
-      { page === 'details' && <Details/>}
+      { page === 'info' && <Announcement/>}
     </>
   );
 }
